417ee9f319a0fd3f28edf72e22e2440c32c7630f
amalabum <eliezermubalama@gmail.com>
2024-01-04 06:49:50 -0800

    commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="customised-style\style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+.app{
+  position: absolute;
+  left:0;
+  top: 0;
+  right:0;
+  bottom:0;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  font-size: 30px;
+  font-family: 'Poppins', sans-serif;
+  flex-direction: column;
+  font-weight: 500;
+}
+.h3{
+   font-weight: 100 !important;
+
+}
+    </style>
+    <title>aidprofen</title>
+
+</head>
+<body>
+    <div class="app">
+         <img src="assets\icons8-maintenance-80.png" alt="">
+        <div >la plateforme est en cours</div>
+        <div class="h3">de construction...</div>
+        
+    </div>
+</body>
+</html>
