48f5da070059478bbc4cf33139fb6373972e5e1a
amalabum <eliezermubalama@gmail.com>
2024-01-04 23:27:19 -0800

    commit

--- myAPI/api.php
+++ myAPI/api.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // Récupération des données POST
 $dataFromKobo = json_decode(file_get_contents('php://input'), true);
 
@@ -8,32 +7,24 @@ $dataFromKobo = json_decode(file_get_contents('php://input'), true);
 
 // Réponse à Kobo Collect
 http_response_code(200);
-echo json_encode(array('message' => 'Données reçues avec succès'));
-
-<?php
-
-// Récupération des données POST
-$dataFromKobo = json_decode(file_get_contents('php://input'), true);
-
-// Traitement des données ici (par exemple, stockage en base de données)
-
-// Réponse à Kobo Collect
-http_response_code(200);
-echo json_encode(array('message' => 'Données reçues avec succès'));
-
-// Enregistrez les données dans un fichier de journal
-$logFilePath = '/myAPI';
+if(!emty($dataFromKobo)){
+    echo json_encode(array('message' => 'Données reçues avec succès'));
+    // Enregistrez les données dans un fichier de journal
+$logFilePath = 'myAPI/fichier.log';
 
 // Créer le fichier s'il n'existe pas
 if (!file_exists($logFilePath)) {
     fopen($logFilePath, 'w');
 }
-
 // Écrire les données dans le fichier
 file_put_contents($logFilePath, "Données reçues : " . json_encode($dataFromKobo) . PHP_EOL, FILE_APPEND);
+}
+else{
+    echo "attente de données";
+}
+
+
 
 ?>
 
-
-?>
 
