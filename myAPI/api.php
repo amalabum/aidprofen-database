945126fd8b7c67b05aea3dbca02b23dd76088431
amalabum <eliezermubalama@gmail.com>
2024-01-04 23:49:56 -0800

    commit

--- myAPI/api.php
+++ myAPI/api.php
@@ -1,38 +1,36 @@
 <?php
-// // Définition des en-têtes de la réponse
-// header('Content-Type: application/json');
+
 // Récupération des données POST
 $inputData = file_get_contents('php://input');
 $dataFromKobo = json_decode($inputData, true);
 
 // Vérifier si la requête a échoué
 if ($dataFromKobo === null && json_last_error() !== JSON_ERROR_NONE) {
-    // Erreur lors du décodage JSON
     http_response_code(400); // Bad Request
     echo json_encode(array('error' => 'Erreur lors du décodage JSON.'));
     exit;
 }
+elseif($dataFromKobo != null){
+    echo json_encode(array('message' => 'Données reçues avec succès'));
+}
+else{
+    echo json_encode(array('message' => 'Données reçues en attente'));
+
+}
 
 // Traitement des données ici (par exemple, stockage en base de données)
 
-// Réponse à Kobo Collect
-http_response_code(200);
-echo json_encode(array('message' => 'Données reçues avec succès'));
+// // Réponse à Kobo Collect
+// http_response_code(200);
+// echo json_encode(array('message' => 'Données reçues avec succès'));
 
-// Enregistrez les données dans un fichier de journal
-$logFilePath = '/myAPI/fichier.log';
+// // Enregistrez les données dans un fichier de journal
+// $logFilePath = '/myAPI/fichier.log';
 
-// Vérifier si le fichier journal peut être écrit
-if (!is_writable($logFilePath)) {
-    error_log("Impossible d'écrire dans le fichier journal.", 0);
-} else {
-    // Écrire les données dans le fichier
-    file_put_contents($logFilePath, "Données reçues : " . $inputData . PHP_EOL, FILE_APPEND);
-}
+// // Écrire les données dans le fichier journal
+// file_put_contents($logFilePath, "Données reçues : " . print_r($dataFromKobo, true) . PHP_EOL, FILE_APPEND);
 
 ?>
 
-
-?>
 
 
