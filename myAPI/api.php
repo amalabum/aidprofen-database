549db6d1597922e0cc63369aff6b3e37c45e7364
amalabum <eliezermubalama@gmail.com>
2024-01-04 23:13:48 -0800

    utilisation d'un fichier log

--- myAPI/api.php
+++ myAPI/api.php
@@ -1,25 +1,18 @@
 <?php
 
+
 // Récupération des données POST
 $dataFromKobo = json_decode(file_get_contents('php://input'), true);
 
 // Traitement des données ici (par exemple, stockage en base de données)
 
-// Définition des en-têtes de la réponse
-header('Content-Type: application/json');
-// Ajoutez d'autres en-têtes selon les besoins de Kobo Collect
-
 // Réponse à Kobo Collect
 http_response_code(200);
 echo json_encode(array('message' => 'Données reçues avec succès'));
 
-// Afficher les données sur l'écran
-echo "datas :";
-echo "</br>"; 
-echo '<h2>Données reçues :</h2>';
-echo '<pre>';
-print_r($dataFromKobo);
-echo '</pre>';
+// Enregistrez les données dans un fichier de journal (ou un autre mécanisme)
+error_log("Données reçues : " . json_encode($dataFromKobo), 3, "myApi/fichier.log");
+
 
 ?>
 
