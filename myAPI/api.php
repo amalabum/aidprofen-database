132260a6012f14242223bda0902855dbb0878418
amalabum <eliezermubalama@gmail.com>
2024-01-05 02:18:26 -0800

    commit

--- myAPI/api.php
+++ myAPI/api.php
@@ -13,11 +13,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'GET')
             echo json_encode(array('message' => 'Données reçues avec succès'));
             http_response_code(200);
 
-            $logFilePath = '/myAPI/fichier.log';
-            // Écrire les données dans le fichier journal
+            $logFilePath = '/myAPI/datas.log';
             file_put_contents($logFilePath, "Données reçues : " . print_r($dataFromKobo, true) . PHP_EOL, FILE_APPEND);
         } else {
-            http_response_code(400); // Bad Request
+            http_response_code(400); 
             echo json_encode(array('error' => 'Erreur lors du décodage JSON.'));
             exit;
         }
@@ -25,7 +24,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'GET')
         echo json_encode(array('message' => 'En attente des données...'));
     }
 } else {
-    // Si la méthode n'est pas POST, renvoyez une erreur
     header('HTTP/1.1 405 Method Not Allowed');
     header('Allow: POST');
     echo json_encode(array('error' => 'Méthode non autorisée'));
