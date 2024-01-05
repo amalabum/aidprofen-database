c9d9e633bc1d636b57dd8a333e6d4e2c29e4ad25
amalabum <eliezermubalama@gmail.com>
2024-01-05 01:53:38 -0800

    commit

--- myAPI/api.php
+++ myAPI/api.php
@@ -2,14 +2,13 @@
 header('Content-Type: application/json');
 header('Accept: application/json');
 header('Access-Control-Allow-Origin: *');
-header('Authorization: 3a0fe7888ff523a586069cb777793642db8551f9');
 header('Access-Control-Allow-Methods: GET, POST, PUT');
 header('Access-Control-Allow-Headers: Origin, Authorization, Content-Type, Accept');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' || $_SERVER['REQUEST_METHOD'] == 'GET') {
     $dataFromKobo = json_decode(file_get_contents('php://input'), true);
 
-    if ($dataFromKobo !== null) {
+    if ($dataFromKobo != null) {
         if (json_last_error() == JSON_ERROR_NONE) {
             echo json_encode(array('message' => 'Données reçues avec succès'));
             http_response_code(200);
